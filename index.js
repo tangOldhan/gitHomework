c2be74c167bea139bfd62083e27a2abcd276c2e3
tangleileo <tanglei1007@gmail.com>
2017-10-24 19:37:48 +0800

    nothing serious

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 window.onload = function(){
   var parent = document.querySelector(".parent");
+  var child = document.querySelector(".child");
   parent.onclick = function(event){
     console.log(event.target);
     console.log(event.currentTarget);
   };
+  child.addEventListener('click',function(event){
+    console.log(event.target);
+    console.log(event.currentTarget);
+  });
+
 };
