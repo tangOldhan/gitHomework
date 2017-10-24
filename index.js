951112aa95296858a234bbf079a22bc01d108fd3
tangleileo <tanglei1007@gmail.com>
2017-10-24 19:09:28 +0800

    currentTarget

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ window.onload = function(){
   var parent = document.querySelector(".parent");
   parent.onclick = function(event){
     console.log(event.target);
+    console.log(event.currentTarget);
   };
 };
